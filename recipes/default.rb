4264ac6d2ea70090651642da10aabae5736e7b8f
Jeremy Seago <seagoj@gmail.com>
2013-08-18 20:16:56 -0500

    Corrects PHP dependency

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "php"
+include_recipe "php5-fpm"
 
 # phpunit/PHPUnit depends on symfony/YAML
 channels = %w{pear.phpunit.de pear.symfony.com}
